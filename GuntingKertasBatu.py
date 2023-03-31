d66533013e3d5df016f86be4a6c9b44d809ae39a
BroManaf <129253337+BroManaf@users.noreply.github.com>
2023-03-31 21:27:09 +0700

    Update GuntingKertasBatu.py

--- GuntingKertasBatu.py
+++ GuntingKertasBatu.py
@@ -11,3 +11,18 @@ def header():
 class gamsut :
     def _init_(self,name):
         self.name = name
+                
+    def playerinput (self):
+        inputuser = int (input (f'Apa pilihan {self.name} (angka)? '))
+        if inputuser > 2 or inputuser < 0:
+            print("Masukkan pilihan dengan benar!\n")
+            return gamsut.playerinput()
+        player = tools[inputuser]
+        print (f'\n{self.name} memilih {player}')
+        return player
+
+    def komputerinput():
+        import random
+        computerinput = random.choice(tools)
+        print (f'Computer memilih {computerinput}')
+        return computerinput
